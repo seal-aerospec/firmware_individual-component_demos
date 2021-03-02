56eaad80c9bc1a91f1bff894cdce159705300bec
Nathan Chung <chungnathan3@gmail.com>
2021-03-01 23:44:10 -0800

    added c02 demo code

---
+++ co2_voc_sensor_demo/co2_voc_sensor_demo.ino
@@ -0,0 +1,36 @@
+
+
+#include "Adafruit_CCS811.h"
+#include <Wire.h>
+
+Adafruit_CCS811 ccs;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("CCS811 test");
+
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+}
+
+void loop() {
+  if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  delay(500);
+}
