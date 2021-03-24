723c9b9231cc0790b67e252a73bb035535de49cb
Nathan Chung <chungnathan3@gmail.com>
2021-03-24 15:21:31 -0700

    added .h and .cpp

--- co2_voc_sensor_demo/co2_voc_sensor_demo.ino
+++ co2_voc_sensor_demo/co2_voc_sensor_demo.ino
@@ -1,36 +1,12 @@
-
-
-#include "Adafruit_CCS811.h"
-#include <Wire.h>
-
-Adafruit_CCS811 ccs;
+#include "co2_voc.h"
 
 void setup() {
   Serial.begin(115200);
-
   Serial.println("CCS811 test");
-
-  if(!ccs.begin()){
-    Serial.println("Failed to start sensor! Please check your wiring.");
-    while(1);
-  }
-
-  // Wait for the sensor to be ready
-  while(!ccs.available());
+  co2_init();
 }
 
 void loop() {
-  if(ccs.available()){
-    if(!ccs.readData()){
-      Serial.print("CO2: ");
-      Serial.print(ccs.geteCO2());
-      Serial.print("ppm, TVOC: ");
-      Serial.println(ccs.getTVOC());
-    }
-    else{
-      Serial.println("ERROR!");
-      while(1);
-    }
-  }
+  std::pair<float, float> ccsRead = getCo2Voc();
   delay(500);
 }
